0bc9bf1649ec0ba1375e5ba0ae8e6fcc0c336280
mattlundin <74123405+mattlundin@users.noreply.github.com>
2022-02-09 21:24:20 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const next = document.querySelector('.right-arrow')
+const back = document.querySelector('.left-arrow')
+const previewContainer = document.querySelector('.img-preview');
+
+const animate
+
+let currentIndex = 1;
+
